2da620aa28362ddb47e6c39c73bcc30d6eec98ee
SSt6708 <stevenliu6708@gmail.com>
2019-09-17 18:08:49 -0700

    finish hastable

--- warmup/wc.c
+++ warmup/wc.c
@@ -4,7 +4,8 @@
 #include "common.h"
 #include "wc.h"
 #include <string.h>
-#include<ctype.h>
+#include <ctype.h>
+#include <sys/malloc.h>
 
 
 typedef struct node{
@@ -21,9 +22,11 @@ struct wc {
 
 };
 
-
+//helper functions
 void fillTable(struct wc* wc, char* userInput);
 int hashKey(char *word, unsigned long tableSize); // generates hashFunctions
+void printEntry( entry* t);
+void deleteEntry( entry* t);
 
 struct wc *
 wc_init(char *word_array, long size)
@@ -31,7 +34,7 @@ wc_init(char *word_array, long size)
 	struct wc *wc;
 
 	wc = (struct wc *)malloc(sizeof(struct wc));
-	assert(wc);
+	assert(wc); 
 
 	//init size of table 
 	wc->table = (entry**) malloc(size * sizeof(entry *));
@@ -99,27 +102,89 @@ void addWord(struct wc *wc, char *word, int index){
 
 void fillTable(struct wc* wc, char* userInput){
 
+	char *lastHold = NULL; //last word being parsed
 
-while (userInput[i] != '\0')
-{
-	if (isspace(userInput[i])){
-		
+	int i = 0;
+	int wordSize = 0;
+	int lastPosition = 0;
+
+	while(userInput[i]!= '\0'){
+
+		if(!isspace(userInput[i])){
+
+			
+			wordSize++;
+		}else{  // sees a space
+
+			if(wordSize != 0){
+				
+				lastHold = (char*) malloc((wordSize+1)*sizeof(char)); //
+				int j = 0;
+
+				while(j < wordSize){
+
+					lastHold[j] = userInput[lastPosition + j];
+				}
+				 lastPosition = i+1;// update the beginning index of the next word
+				
+				int Key = hashKey(lastHold, wc->tableSize);
+				addWord(wc, lastHold, Key);
+				lastHold = NULL;
+				wordSize = 0;
+				i++;
+			}
+			i++;
+		}
+
+		i++;
+
 	}
-}
 
 
 
 }
 
+void printEntry( entry* t){
+
+	while(t->next != NULL){
+		printf("%s:%d\n", t->word, t->count);
+		t = t->next;
+
+	}
+
+}
+void deleteEntry(entry* t){
+
+	if(t == NULL){
+		return;
+	}
+	deleteEntry(t->next);
+	free(t->word);
+	free(t);
+}
 void
 wc_output(struct wc *wc)
 {
-	TBD();
+	int i;
+	for(i = 0; i < wc->tableSize; i++){
+		if(wc->table[i] != NULL){
+			printEntry(wc->table[i]);
+		}
+	}
 }
 
 void
 wc_destroy(struct wc *wc)
 {
-	TBD();
+	int i;
+	for(i = 0; i < wc->tableSize ; i ++){
+		if(wc->table[i] != NULL){
+			deleteEntry(wc->table[i]);
+
+
+		}
+
+	}
+	free(wc->table);
 	free(wc);
 }
